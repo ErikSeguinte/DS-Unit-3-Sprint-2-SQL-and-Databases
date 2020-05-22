957825b1b1423778e9cc1b711433cc6673aa0011
Erik Seguinte <erikseguinte@me.com>
2020-05-21 23:09:15 -0700

    finished part 1

--- module1-introduction-to-sql/assignment.py
+++ module1-introduction-to-sql/assignment.py
@@ -1,12 +1,12 @@
 import sqlite3
 from pathlib import Path
 
-path = Path('rpg_db.sqlite3')
+path = Path("rpg_db.sqlite3")
 
 conn = sqlite3.connect(path)
 # conn.row_factory = sqlite3.Row
 c = conn.cursor()
-query = 'select * from Armory_item;'
+query = "select * from Armory_item;"
 
 result = c.execute(query).fetchall()
 
@@ -21,35 +21,29 @@ result = c.execute(q).fetchone()
 
 print(f"The number of characters: {result[0]}")
 
-classes = [
-    'cleric',
-    'fighter',
-    'mage',
-    'necromancer',
-    'thief'
-    ]
+classes = ["cleric", "fighter", "mage", "necromancer", "thief"]
 
 for char_class in classes:
-    q = f'''select 
+    q = f"""select 
 	            count(DISTINCT character_ptr_id)
 
             from charactercreator_{char_class};
-        '''
+        """
     try:
         result = c.execute(q).fetchone()
     except sqlite3.OperationalError:
-        q = f'''select 
+        q = f"""select 
                     count(DISTINCT mage_ptr_id) as total
 
                 from charactercreator_{char_class};
-            '''
+            """
         result = c.execute(q).fetchone()
 
     print(f"The number of {char_class} characters: {result[0]}")
 
-    
+
 # Items
-q ="""
+q = """
     select 
         count(distinct item_id)
     from armory_item;
@@ -60,7 +54,7 @@ item_count = result[0]
 print()
 print(f"Total number of items: {item_count}")
 
-q ="""
+q = """
     select 
         count(distinct item_ptr_id)
     from armory_weapon;
@@ -73,23 +67,64 @@ print(f"Total number of non-weapons: {item_count - weapon_count}")
 
 
 q = """
-    select 
+    select
         items.character_id as character,
         characters.name,
         count(item_id) as number_of_items
-
-    from 
+    from
         charactercreator_character_inventory as items,
         charactercreator_character as characters
-        
-    where characters.character_id = items.character_id
-
-    group by character
-
-    limit 10;
+    where
+        characters.character_id = items.character_id
+    group by
+        character
+    limit
+        10
     """
 results = c.execute(q).fetchall()
 
-print()
+print("\nFirst 20 character inventories:")
 for i in results:
     print(i)
+
+sq = q
+q = f"""
+    select
+    avg(number_of_items)
+    from ({sq});
+    """
+
+results = c.execute(q).fetchone()
+
+print(f"Average item count: {results[0]}")
+
+
+q = """
+SELECT
+    characters.name,
+    count (items.item_id) as number_of_weapons
+FROM
+    charactercreator_character as characters
+    JOIN charactercreator_character_inventory as inv on characters.character_id = inv.character_id
+    join armory_item as items on inv.item_id = items.item_id
+    join armory_weapon as weapons on items.item_id = weapons.item_ptr_id
+group by
+    inv.character_id
+limit
+    20
+"""
+
+results = c.execute(q).fetchall()
+
+print("\nFirst 20 weapon inventories:")
+for i in results:
+    print(i)
+
+sq = q
+q = f"""
+    select
+    avg(number_of_weapons)
+    from ({sq});
+    """
+results = c.execute(q).fetchone()
+print(f"Average weapon count: {results[0]}")
