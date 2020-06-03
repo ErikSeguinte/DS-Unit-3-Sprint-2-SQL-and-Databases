75821b56374075ce19855f82124d468c4fba945e
Erik Seguinte <erikseguinte@me.com>
2020-06-03 00:00:57 -0700

    Using Context managers

--- module4-acid-and-database-scalability-tradeoffs/load_rpg_data.py
+++ module4-acid-and-database-scalability-tradeoffs/load_rpg_data.py
@@ -1,49 +1,50 @@
 import pymongo, sqlite3
 import pandas as pd
 import numpy as np
+import contextlib
 from pathlib import Path
 
 path = Path("module1-introduction-to-sql/rpg_db.sqlite3")
 
-with sqlite3.connect(path) as con:
+with contextlib.closing(sqlite3.connect(path)) as con:
 
-    c = con.cursor()
+    with con as c:
 
-    q = """
-    select 
-        count(DISTINCT character_id)
+        q = """
+        select 
+            count(DISTINCT character_id)
 
-    from charactercreator_character;
-    """
+        from charactercreator_character;
+        """
 
-    result = c.execute(q).fetchone()
-    print(result)
+        result = c.execute(q).fetchone()
+        print(result)
 
-    char_tables_list= [
-        'character',
-        'character_inventory',
-        'mage',
-        'necromancer',
-        'thief',
-        'cleric',
-        'fighter',
-    ]
+        char_tables_list= [
+            'character',
+            'character_inventory',
+            'mage',
+            'necromancer',
+            'thief',
+            'cleric',
+            'fighter',
+        ]
 
-    tables = dict()
+        tables = dict()
 
-    for s in char_tables_list:
-        tables[s] = pd.read_sql(f"select * from charactercreator_{s}", con)
+        for s in char_tables_list:
+            tables[s] = pd.read_sql(f"select * from charactercreator_{s}", con)
 
-        print(tables[s].head())
+            print(tables[s].head())
 
-    armory_tables_list = [
-        'item',
-        'weapon'
-    ]
+        armory_tables_list = [
+            'item',
+            'weapon'
+        ]
 
-    armory = dict()
-    for s in armory_tables_list:
-        armory[s] = pd.read_sql(f"select * from armory_{s}", con)
-        print(armory[s].head())
+        armory = dict()
+        for s in armory_tables_list:
+            armory[s] = pd.read_sql(f"select * from armory_{s}", con)
+            print(armory[s].head())
 
 
